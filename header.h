a8f3e36eb9905bf6b16abdbe80e1bf0c79ec12a5
ddojugi <55527862+ddojugi@users.noreply.github.com>
2021-06-11 14:34:44 +0900

    Create header.h

---
+++ header.h
@@ -0,0 +1,14 @@
+//header.h
+ 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/select.h>
+ 
+#define PORT 20162
+#define BUFFER_SIZE 100
+#define LISTEN_QUEUE_SIZE 5
